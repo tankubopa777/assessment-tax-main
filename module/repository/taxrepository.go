d341f5751cfa4b4beaa6fe6e75fcd802cc91f157
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-04-22 04:29:39 +0700

    update correct logic

--- module/repository/taxrepository.go
+++ module/repository/taxrepository.go
@@ -2,8 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
-	"math"
 
 	"github.com/tankubopa777/assessment-tax/module/models"
 )
@@ -19,49 +17,63 @@ func NewPostgresTaxRepository(db *sql.DB) *PostgresTaxRepository {
 func (r *PostgresTaxRepository) CalculateTax(input models.TaxCalculationInput) (models.TaxCalculationResult, error) {
     taxBrackets := []models.TaxBracket{
         {LowerBound: 0, UpperBound: 150000, Rate: 0},
-        {LowerBound: 150001, UpperBound: 500000, Rate: 0.1},
-        {LowerBound: 500001, UpperBound: 1000000, Rate: 0.15},
-        {LowerBound: 1000001, UpperBound: 2000000, Rate: 0.2},
-        {LowerBound: 2000001, UpperBound: -1, Rate: 0.35},
+        {LowerBound: 150000, UpperBound: 500000, Rate: 0.1},
+        {LowerBound: 500000, UpperBound: 1000000, Rate: 0.15},
+        {LowerBound: 1000000, UpperBound: 2000000, Rate: 0.2},
+        {LowerBound: 2000000, UpperBound: -1, Rate: 0.35},
     }
 
-    var totalDeductions float64 = 60000 
+    var totalDeductions float64 = 60000
+    var donationDeduction float64 = 0
+    var kReceiptDeduction float64 = 0
 
     for _, allowance := range input.Allowances {
-        if allowance.AllowanceType == "donation" {
-            totalDeductions += math.Min(allowance.Amount, 100000)
-        } else if allowance.AllowanceType == "k-receipt" {
-            totalDeductions += math.Min(allowance.Amount, 50000)
+        switch allowance.AllowanceType {
+        case "donation":
+            donationDeduction += allowance.Amount
+            if donationDeduction > 100000 {
+                donationDeduction = 100000
+            }
+        case "k-receipt":
+            kReceiptDeduction += allowance.Amount
+            if kReceiptDeduction > 50000 {
+                kReceiptDeduction = 50000
+            }
         }
     }
+
+    totalDeductions += donationDeduction + kReceiptDeduction
 
     taxableIncome := input.TotalIncome - totalDeductions
-    fmt.Println("taxableIncome: ", taxableIncome)
-    var taxAmount float64
-    var taxDetails []models.TaxLevelDetail
 
-    for _, bracket := range taxBrackets {
-        levelDescription := fmt.Sprintf("%d-%d", bracket.LowerBound, bracket.UpperBound)
-        if bracket.UpperBound == -1 {
-            levelDescription = fmt.Sprintf("%d ขึ้นไป", bracket.LowerBound)
-        }
-        taxDetails = append(taxDetails, models.TaxLevelDetail{
-            Level: levelDescription,
-            Tax:   0,
-        })
-    }
+    var totalTax float64
+    taxDetails := make([]models.TaxLevelDetail, len(taxBrackets))
 
     for i, bracket := range taxBrackets {
-        if taxableIncome > float64(bracket.LowerBound) && (bracket.UpperBound == -1 || taxableIncome <= float64(bracket.UpperBound)) {
-            taxAmount = (taxableIncome - (float64(bracket.LowerBound)-1)) * bracket.Rate
-            taxDetails[i].Tax = math.Round(taxAmount*100) / 100
-            break
+        upperLimit := float64(bracket.UpperBound)
+        if bracket.UpperBound == -1 {
+            upperLimit = taxableIncome
+        } else if taxableIncome < upperLimit {
+            upperLimit = taxableIncome
+        }
+
+        lowerBound := float64(bracket.LowerBound)
+        incomeInBracket := upperLimit - lowerBound
+        taxForBracket := 0.0
+
+        if taxableIncome > lowerBound && incomeInBracket > 0 {
+            taxForBracket = incomeInBracket * bracket.Rate
+            totalTax += taxForBracket
+        }
+
+        // Populate the details for all brackets, even if tax is zero
+        taxDetails[i] = models.TaxLevelDetail{
+            Level: bracket.String(),
+            Tax:   taxForBracket,
         }
     }
 
-    taxAmount = math.Round(taxAmount*100) / 100
-    finalTax := taxAmount - input.WHT
-
+    finalTax := totalTax - input.WHT
     var taxRefund float64
     if finalTax < 0 {
         taxRefund = -finalTax
@@ -69,8 +81,9 @@ func (r *PostgresTaxRepository) CalculateTax(input models.TaxCalculationInput) (
     }
 
     return models.TaxCalculationResult{
-        Tax:            finalTax,
-        TaxRefund:      taxRefund,
+        Tax:             finalTax,
+        TaxRefund:       taxRefund,
         TaxLevelDetails: taxDetails,
     }, nil
 }
+
