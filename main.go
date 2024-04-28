02f7add157b2dfce7ffc35224612b2e335f355fb
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-04-28 20:39:52 +0700

    update port

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	
 	// Graceful shutdown
 	go func() {
-		if err := e.Start(":5050"); err != nil && err != http.ErrServerClosed {
+		if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
 			e.Logger.Fatal("shutting down the server", err)
 		}
 	}()
